99bf0c33719d2432aa4cd7a9962e53a2a912d6fc
lucasalmeida <lac.lucas96@gmail.com>
2022-11-15 18:36:56 -0300

    Removes dialog from table map, still looping

--- src/components/EditDialogForm.tsx
+++ src/components/EditDialogForm.tsx
@@ -5,90 +5,97 @@ import { Car } from "../interfaces/ICar";
 import { X } from "./Icons/X";
 
 interface IEditDialogForm {
-  isOpen: boolean;
-  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  car: Car;
+  openDialog: (car: Car) => void;
+  closeDialog: () => void;
+  isOpen: {
+    open: boolean;
+    car: { license_plate: string; model: string };
+  };
 }
 
-// TODO: Fix multiple re renders when dialog opens
-
-export function EditDialogForm({ isOpen, setIsOpen, car }: IEditDialogForm) {
+export function EditDialogForm({
+  isOpen,
+  openDialog,
+  closeDialog,
+}: IEditDialogForm) {
   const licensePlate = useRef() as MutableRefObject<HTMLInputElement>;
   const carModel = useRef() as MutableRefObject<HTMLInputElement>;
   const form = useRef() as MutableRefObject<HTMLFormElement>;
 
-  console.log(car);
+  console.log(isOpen.car);
 
-  const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  // const handleFormSubmit = (event: FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
 
-    const data = {
-      license_plate: licensePlate.current.value,
-      model: carModel.current.value,
-    };
+  //   const data = {
+  //     license_plate: licensePlate.current.value,
+  //     model: carModel.current.value,
+  //   };
 
-    axios.put(`http://localhost:3000/api/car/${car.id}`, data, {
-      headers: {
-        "Content-Type": "application/json; charset=UTF-8",
-        Accept: "Token",
-        "Access-Control-Allow-Origin": "*",
-      },
-    });
+  //   axios.put(`http://localhost:3000/api/car/${car.id}`, data, {
+  //     headers: {
+  //       "Content-Type": "application/json; charset=UTF-8",
+  //       Accept: "Token",
+  //       "Access-Control-Allow-Origin": "*",
+  //     },
+  //   });
 
-    form.current.reset();
-    setIsOpen(false);
-  };
+  //   form.current.reset();
+  //   closeDialog();
+  // };
 
   return (
-    <Dialog
-      open={isOpen}
-      onClose={() => setIsOpen(false)}
-      className="fixed top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 rounded-md bg-slate-200"
-    >
-      <Dialog.Panel>
-        <div className="relative p-8">
-          <button
-            type="button"
-            onClick={() => setIsOpen(false)}
-            className="absolute right-2 top-2"
-          >
-            <X width={32} height={32} />
-          </button>
-          <form name="insert" ref={form} onSubmit={handleFormSubmit}>
-            <legend>New Car</legend>
-            <div className="flex flex-col gap-2">
-              <div className="flex gap-1">
-                <label>Model: </label>
-                <input
-                  ref={carModel}
-                  type="text"
-                  name="model"
-                  id="model"
-                  defaultValue={car.model}
-                  required
-                />
-              </div>
-              <div className="flex gap-1">
-                <label>License Plate: </label>
-                <input
-                  ref={licensePlate}
-                  type="text"
-                  name="license_plate"
-                  id="license_plate"
-                  defaultValue={car.license_plate}
-                  required
-                />
-              </div>
-            </div>
+    <>
+      <Dialog
+        open={isOpen.open}
+        onClose={closeDialog}
+        className="fixed top-1/2 left-1/2 z-50 -translate-x-1/2 -translate-y-1/2 rounded-md bg-slate-200"
+      >
+        <Dialog.Panel>
+          <div className="relative p-8">
             <button
-              type="submit"
-              className="rounded-md bg-slate-100 py-1 px-3 uppercase"
+              type="button"
+              onClick={closeDialog}
+              className="absolute right-2 top-2"
             >
-              Update
+              <X width={32} height={32} />
             </button>
-          </form>
-        </div>
-      </Dialog.Panel>
-    </Dialog>
+            <form name="insert" ref={form}>
+              <legend>New Car</legend>
+              <div className="flex flex-col gap-2">
+                <div className="flex gap-1">
+                  <label>Model: </label>
+                  <input
+                    ref={carModel}
+                    type="text"
+                    name="model"
+                    id="model"
+                    defaultValue={isOpen.car?.model}
+                    required
+                  />
+                </div>
+                <div className="flex gap-1">
+                  <label>License Plate: </label>
+                  <input
+                    ref={licensePlate}
+                    type="text"
+                    name="license_plate"
+                    id="license_plate"
+                    defaultValue={isOpen.car?.license_plate}
+                    required
+                  />
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="rounded-md bg-slate-100 py-1 px-3 uppercase"
+              >
+                Update
+              </button>
+            </form>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
+    </>
   );
 }
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,12 +1,18 @@
 import axios from "axios";
-import { useState, useEffect } from "react";
+import { useState, useEffect, FormEvent } from "react";
 import { Car } from "../interfaces/ICar";
 import { EditDialogForm } from "./EditDialogForm";
 
 export function Table() {
   const [cars, setCars] = useState<Car[]>([]);
-
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [selectedCar, setSelectedCar] = useState({
+    license_plate: "",
+    model: "",
+  });
+  const [isOpen, setIsOpen] = useState({
+    open: false,
+    car: selectedCar,
+  });
 
   useEffect(() => {
     axios
@@ -21,48 +27,57 @@ export function Table() {
     }
   };
 
+  const openDialog = (car: Car) => {
+    setSelectedCar(car);
+    setIsOpen({ open: true, car: selectedCar });
+  };
+  const closeDialog = () => {
+    setSelectedCar({ license_plate: "", model: "" });
+    setIsOpen({ open: false, car: selectedCar });
+  };
+
   return (
-    <table className="w-full table-auto">
-      <thead>
-        <tr>
-          <td>id</td>
-          <td>license_plate</td>
-          <td>model</td>
-          <td>actions</td>
-        </tr>
-      </thead>
-      <tbody>
-        {cars.map((car) => (
-          <tr key={car.id}>
-            <td>{car.id}</td>
-            <td>{car.license_plate}</td>
-            <td>{car.model}</td>
-            <td className="flex gap-2">
-              <button
-                type="button"
-                onClick={() => handleDeleteRow(car.id)}
-                className="w-16 rounded-md bg-red-500 py-1 text-white hover:bg-red-600"
-              >
-                Delete
-              </button>
-              <button
-                type="button"
-                onClick={() => setIsOpen(true)}
-                className="w-16 rounded-md bg-blue-500 py-1 text-white hover:bg-blue-600"
-              >
-                Edit
-              </button>
-              {isOpen ? (
-                <EditDialogForm
-                  isOpen={isOpen}
-                  setIsOpen={setIsOpen}
-                  car={car}
-                />
-              ) : null}
-            </td>
+    <>
+      <table className="w-full table-auto">
+        <thead>
+          <tr>
+            <td>id</td>
+            <td>license_plate</td>
+            <td>model</td>
+            <td>actions</td>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {cars.map((car) => (
+            <tr key={car.id}>
+              <td>{car.id}</td>
+              <td>{car.license_plate}</td>
+              <td>{car.model}</td>
+              <td className="flex gap-2">
+                <button
+                  type="button"
+                  onClick={() => handleDeleteRow(car.id)}
+                  className="w-16 rounded-md bg-red-500 py-1 text-white hover:bg-red-600"
+                >
+                  Delete
+                </button>
+                <button
+                  type="button"
+                  onClick={() => openDialog(car)}
+                  className="w-16 rounded-md bg-blue-500 py-1 text-white hover:bg-blue-600"
+                >
+                  Edit
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <EditDialogForm
+        isOpen={isOpen}
+        openDialog={openDialog}
+        closeDialog={closeDialog}
+      />
+    </>
   );
 }
